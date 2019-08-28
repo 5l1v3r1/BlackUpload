95e9c0d1e290913d4a0af9a17df9dd011c45b637
Black.Hacker <farisksa79@gmail.com>
2019-08-28 04:23:03 +0300

    Update report.php

--- report.php
+++ report.php
@@ -6,14 +6,14 @@ include 'configuration/socialmedia.php';
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $fname = isset($_POST['firstname']) ? $_POST['firstname']: '';
     $lname = isset($_POST['lastname']) ? $_POST['lastname']: '';
-    $name = $fname.' '.$lname;
+    $uName = $fname.' '.$lname;
     $uEmail = isset($_POST['email']) ? $_POST['email']: '';
     $subject = isset($_POST['subject']) ? $_POST['subject']: '';
     $message = isset($_POST['message']) ? $_POST['message']: '';
 
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-    $headers .= "From: '.$name.'<'.$uEmail.'>\r\n";
+    $headers .= "From: '.$uName.'<'.$uEmail.'>\r\n";
     if (mail($email, $subject, $message, $headers)) {
         $ok = "Thank you for keeping our service safe.";
     } else {
